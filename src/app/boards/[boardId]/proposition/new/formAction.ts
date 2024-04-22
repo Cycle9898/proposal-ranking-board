8b090bacc6f7ac0e64bae19ba6a7ccf91c86e3c5
Cycle9898 <cycle9898@gmail.com>
2024-04-22 19:29:28 +0200

    fixing proposition formAction that avoid page refresh with the freshest data

--- src/app/boards/[boardId]/proposition/new/formAction.ts
+++ src/app/boards/[boardId]/proposition/new/formAction.ts
@@ -8,19 +8,19 @@ const TitleScheme = z.string().min(1).max(200);
 export const submitPropositionForm = async (proposition: string, boardId: number) => {
 	const propositionTitle = TitleScheme.parse(proposition);
 
-	// Get client IP address and save proposition in database
-	fetch("https://api.ipify.org?format=json")
+	// Get client IP address
+	const clientIpAddress = fetch("https://api.ipify.org?format=json")
 		.then(response => response.json())
-		.then(async data => {
-			await prisma.proposition.create({
-				data: {
-					title: propositionTitle,
-					boardId: boardId,
-					ip: data.ip
-				}
-			});
-		})
+		.then(data => data.ip)
 		.catch(error => {
 			throw new Error(error);
 		});
+
+	await prisma.proposition.create({
+		data: {
+			title: propositionTitle,
+			boardId: boardId,
+			ip: String(clientIpAddress)
+		}
+	});
 };
