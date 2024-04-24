1e16f056e302e9ef00effdbdeef2ac43c64bb582
Cycle9898 <cycle9898@gmail.com>
2024-04-24 18:14:21 +0200

    changing max characters value in title zod scheme for new board and new proposition server actions

--- src/app/boards/[boardId]/new-proposition/newProposition.action.ts
+++ src/app/boards/[boardId]/new-proposition/newProposition.action.ts
@@ -4,7 +4,7 @@ import { z } from "zod";
 import { prisma } from "@/db/prisma";
 import { getIpAddress } from "@/utils/getIpAddress";
 
-const TitleScheme = z.string().min(1).max(200);
+const TitleScheme = z.string().min(1).max(190);
 
 export const submitPropositionForm = async (proposition: string, boardId: number) => {
 	const propositionTitle = TitleScheme.parse(proposition);
--- src/app/boards/new/newBoard.action.ts
+++ src/app/boards/new/newBoard.action.ts
@@ -3,7 +3,7 @@
 import { z } from "zod";
 import { prisma } from "@/db/prisma";
 
-const TitleScheme = z.string().min(1).max(200);
+const TitleScheme = z.string().min(1).max(190);
 
 export const submitBoardForm = async (title: string) => {
 	const boardTitle = TitleScheme.parse(title);
