224cfb4e7e9711e2f872141741746a3f4a6e3a2b
Cycle9898 <cycle9898@gmail.com>
2024-05-03 16:38:03 +0200

    adding pagination for propositions

--- src/app/boards/[boardId]/page.tsx
+++ src/app/boards/[boardId]/page.tsx
@@ -1,15 +1,26 @@
 import { prisma } from "@/db/prisma";
 import { Button } from "@/components/features/form/Button";
 import Proposition from "@/components/features/proposition/PropositionLine";
+import PaginationCommands from "@/components/features/pagination/PaginationCommands";
 
 type BoardPageProps = {
 	params: { boardId: string };
+	searchParams: { [key: string]: string | string[] | undefined };
 };
 
-async function BoardPage({ params }: BoardPageProps) {
+async function BoardPage({ params, searchParams }: BoardPageProps) {
 	const boardId = parseInt(params.boardId);
+	const pageNb = Number(searchParams.page ?? 0);
+	const propositionsPerPage = 4;
 
+	const totalPropositionsNumber = await prisma.proposition.count({
+		where: {
+			boardId: boardId
+		}
+	});
 	const propositions = await prisma.proposition.findMany({
+		take: propositionsPerPage,
+		skip: Math.max(0, pageNb * propositionsPerPage),
 		where: {
 			boardId: boardId
 		},
@@ -30,18 +41,25 @@ async function BoardPage({ params }: BoardPageProps) {
 	});
 
 	return (
-		<div className="flex flex-col gap-6">
+		<div className="flex-1 flex flex-col gap-6">
 			<Button as="a" href={`/boards/${boardId}/new-proposition`} className="self-end">
 				Suggest a proposition
 			</Button>
 
-			<ul className="flex flex-col gap-4">
+			<ul className="flex-1 flex flex-col gap-4">
 				{propositions.map(proposition => (
 					<li key={proposition.id}>
 						<Proposition voteCount={proposition._count.vote} {...proposition} />
 					</li>
 				))}
 			</ul>
+
+			<PaginationCommands
+				className="self-center"
+				totalPage={Math.ceil(totalPropositionsNumber / propositionsPerPage)}
+				pageNb={pageNb}
+				baseUrl={`/boards/${boardId}`}
+			/>
 		</div>
 	);
 }
