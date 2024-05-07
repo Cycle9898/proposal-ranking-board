a693ab210aa12f9807cddb2827dc89a66b27a2c2
Cycle9898 <cycle9898@gmail.com>
2024-05-07 18:01:18 +0200

    fixing a typo in the main title

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -4,7 +4,7 @@ function Header() {
 	return (
 		<header className="text-center py-10 border-black dark:border-white border-b-2">
 			<Link href="/" aria-label="Link to homepage">
-				<h1 className="font-bold text-6xl">Proposal Ranking Board</h1>
+				<h1 className="font-bold text-6xl">Proposal Ranking Boards</h1>
 			</Link>
 		</header>
 	);
